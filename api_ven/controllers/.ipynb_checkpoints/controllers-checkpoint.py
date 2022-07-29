8ac1a70a4e9d3e621f7fa30c91aac11262e0fd41
yohan-kuncoro <yohan.kuncoro@boonsoftware.com>
2022-07-29 04:48:35 +0000

    controller

--- api_ven/controllers/.ipynb_checkpoints/controllers-checkpoint.py
+++ api_ven/controllers/.ipynb_checkpoints/controllers-checkpoint.py
@@ -25,12 +25,41 @@ from odoo import http
 from odoo.http import request, Response
 from datetime import datetime
 
-@http.route('/api_sbt_inv/api_sbt_inv/', auth='public')
+@http.route('/api_sbt_inv/api_sbt_inv/', auth='user')
 def getRecord(self, model, field, wms):
         record = request.env[model].search([(field,'=',wms)])
         if record[field] == wms:
             return record['id']
         else:
             return -1
-        
 
+
+@http.route('/api/createasn', type='json', auth='user', methods=['POST'])
+def post_rcpt(self, rcpt):
+        created = 0
+        error = {}
+        warn_cnt = 1
+        rcpt_lines = []
+        is_error = False
+        response_msg = "Failed to create GRN!"
+        message = {}
+        line_details = []
+        is_partial = False
+        
+        try:
+            for rec in rcpt:
+                if rec['poNo'] == "":
+                    error["Error"] = "Field ownerReference is blank"
+                    is_error = True
+                break
+
+                po = self.getRecord(model="purchase.order", field="name", wms=rec['poNo'])
+                if po == -1:
+                    error["Error"] = "poNo does not exist"
+                    is_error = True
+                break
+        except Exception as e:
+            error["Error"] = str(e)
+            is_error = True
+            
+        return is_error
