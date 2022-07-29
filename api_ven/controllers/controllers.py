2b12d6f20f2c067bf3aebc5806adc89443b84bb3
yohan-kuncoro <yohan.kuncoro@boonsoftware.com>
2022-07-29 03:39:38 +0000

    mvc wilson

--- api_ven/controllers/controllers.py
+++ api_ven/controllers/controllers.py
@@ -53,6 +53,31 @@ def getRecord(self, model, field, wms):
         line_details = []
         is_partial = False
         
+#         try:
+#             api_log = request.env['api_ven.api_ven'].create({
+#                 'status': 'new',
+#                 'created_date': datetime.now(),
+#                 'incoming_msg': ap,
+#                 'message_type': 'ap'
+#             })
+#             api_log['status'] = 'process'
+#         except:
+#             error['Error'] = str(e)
+#             is_error = True
+            
+#         try:
+#             api_log['incoming_txt'] = request.env['ir.attachment'].create({
+#                 'name': str(api_log['name']) + '_in.txt',
+#                 'type': 'binary',
+#                 'datas': base64.b64encode(bytes(str(ap), 'utf-8')),
+#                 'res_model': 'api_ven.api_ven',
+#                 'res_id': api_log['id'],
+#                 'mimetype': 'text/plain'
+#             })
+#         except Exception as e:
+#             error['Error'] = str(e)
+#             is_error = True
+        
 #        try:
         for rec in rcpt:
             #check poNo
@@ -91,136 +116,6 @@ def getRecord(self, model, field, wms):
                     is_error = True
                     break
 
-            #Receipt Line
-            for line in rec['details']:
-                temp_product = 0
-
-                #ownerReference
-                if line['ownerReference'] == "":
-                    error["Error"] = "Field ownerReference is blank"
-                    is_error = True
-                    break
-
-                #inwardLineOptChar1
-                if line['inwardLineOptChar1'] == "":
-                    error["Error"] = "Field inwardLineOptChar1 is blank"
-                    is_error = True
-                    break
-
-                #product
-                if line['product'] == "":
-                    error["Error"] = "Field product is blank"
-                    is_error = True
-                    break
-
-                temp_product = self.getRecord(model="product.product", field="default_code", wms=line['product'])
-                if temp_product == -1:
-
-                    created_product = request.env['product.product'].create({
-                        "type": "product",
-                        "default_code": line['product'],
-                        "name": line['product'],
-                        "tracking": "lot",
-                        "use_expiration_date": 1,
-                        "company_id": 1
-                    })
-
-                    temp_product = created_product['id']
-
-                    warn_str = "Message " + str(warn_cnt)
-                    error[warn_str] = "Product " + line['product'] + " has been created"
-                    warn_cnt += 1
-
-                for det in line['lineDetails']:
-
-                    #Check quantityReceived
-                    if det['quantityReceived'] == "":
-                        error["Error"] = "Field quantityReceived is blank"
-                        is_error = True
-                        break
-
-                    #Check expiryDate
-                    if det['expiryDate'] == "":
-                        expiry_date = ""
-                    else:
-                        try:
-                            expiry_date = datetime.strptime(det['expiryDate'], '%d/%m/%Y').date()
-                        except ValueError:
-                            error["Error"] = "Wrong date format on expiryDate"
-                            is_error = True
-                            break
-
-                    #Check stockStatusCode
-                    if det['stockStatusCode'] == "":
-                        error["Error"] = "Field stockStatusCode is blank"
-                        is_error = True
-                        break
-
-                    #Check lotNo
-                    if det['lotNo'] == "":
-                        error["Error"] = "Field lotNo is blank"
-                        is_error = True
-                        break
-
-                    temp_lot = request.env["stock.production.lot"].search(['&',("product_id",'=',temp_product),("name", '=', det['lotNo'])])
-                    if temp_lot['name'] != det['lotNo']:
-                        temp_lot = request.env['stock.production.lot'].create({
-                            "product_id": temp_product,
-                            "name": det["lotNo"],
-                            "company_id": 1
-                        })
-
-                    #Create Line Detail
-                    line_detail = request.env['stock.move.line'].create({
-                        "product_id": temp_product,
-                        "product_uom_id": 1,
-                        "location_id": 4,
-                        "location_dest_id": 8,
-                        "lot_id": temp_lot['id'],
-                        "expiration_date": expiry_date,
-                        "qty_done": det["quantityReceived"],
-                        "company_id": 1,
-                        "state": "done"
-                    })
-
-                    line_details.append(line_detail['id'])
-
-                #Get existing receipt line data based on poNo and lineOptChar1
-                receipt_line = request.env['stock.move'].search(['&',('origin','=',rec['poNo']),('x_studio_opt_char_1', '=', line["inwardLineOptChar1"])])
-                if receipt_line['origin'] != rec['poNo']:
-                    error["Error"] = "Stock Move not found"
-                    is_error = True
-                    break
-                    
-                #Get previous receipt line detail data
-                existing_detail = []
-                for i in receipt_line['move_line_nosuggest_ids']:
-                    existing_detail.append(i['id'])
-                    
-                #Merge new line details from JSON and existing line details
-                line_details += existing_detail
-                
-                #Update line details data
-                receipt_line['move_line_nosuggest_ids'] = line_details
-                
-                #Check partial receipt
-                if receipt_line['product_uom_qty'] == receipt_line['quantity_done']:
-                    receipt_line['state'] = 'done'
-                else:
-                    is_partial = True
-
-
-                if is_error == True:
-                    break
-
-            if is_error == True:
-                break
-            
-            receipt_header['date_done'] = receipt_date
-            receipt_header['x_studio_document_trans_code'] = rec["documentTransCode"]
-            
-            if is_partial == False:
-                receipt_header['state'] = 'done'
 
             response_msg = "GRN updated successfully"
 #        except Exception as e:
