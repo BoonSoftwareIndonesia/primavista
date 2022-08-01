9adaa8906200ed293efeeadd036a74f16563bac4
yohan-kuncoro <yohan.kuncoro@boonsoftware.com>
2022-08-01 05:04:48 +0000

    model asn

--- api_ven/models/.ipynb_checkpoints/models-checkpoint.py
+++ api_ven/models/.ipynb_checkpoints/models-checkpoint.py
@@ -46,53 +46,46 @@ class api_ven(models.Model):
 class ApiController(models.Model):
     _inherit = "purchase.order"
     
-    def api_dw_po(self):
+    def api_dw_po(self, record):
         apiurl = "https://cloud1.boonsoftware.com/avi-trn-symphony-api/createasn"
         
-#         line_no = 1
-#         po_lines = []
+        line_no = 1
+        po_lines = []
         
-#         for line in record['order_line']:
-#             line['x_studio_opt_char_1'] = str(line_no)
+        for line in record['order_line']:
+            line['x_studio_opt_char_1'] = str(line_no)
             
-#             po_line = {
-#                 "inwardLineOptChar1": line['x_studio_opt_char_1'],
-#                 "inwardLineOptChar2": "" if line['x_studio_opt_char_2'] == False else line['x_studio_opt_char_2'],
-#                 "product": record['product_id']['name'],
-#                 "quantityOrdered": str(line['product_qty']),
-#                 "uomCode": line['product_uom']['name'],
-#                 "stockStatusCode": "" if line['x_studio_stock_status_code'] == False else line['x_studio_stock_status_code']
-#             }
-#             line_no += 1
+            po_line = {
+                "inwardLineOptChar1": line['x_studio_opt_char_1'],
+                "inwardLineOptChar2": "",
+                "product": record['product_id']['name'],
+                "quantityOrdered": str(line['product_qty']),
+                "uomCode": line['product_uom']['name'],
+                "stockStatusCode": "" if line['x_studio_stock_status_code'] == False else line['x_studio_stock_status_code']
+            }
+            line_no += 1
             
-#             po_lines.append(po_line)
+            po_lines.append(po_line)
         
         
         payload = {
             "accessToken": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiJpZCIsImlhdCI6MTYxMTYzNzI3NCwic3ViIjoiaWQiLCJpc3MiOiJodHRwOi8vbG9jYWxob3N0IiwiYXVkIjoib2N0cyIsImV4cCI6MTYxMTcyMzY3NH0.bB2S1bNFxf_D0s8Fp2BGTXNc9CRNjEiRqyWFBNDzZ4c",
             "asn": [
                 {
-                     "ownerReferences":"",
-                     "poNo":"15220014721",
-                     "supplierReferences":"V-80",
-                     "sender":"VITA HEALTH INDONESIA, PT",
-                     "documentTransCode":"PODR",
-                     "ownerCode":"VITAHEALTH",
-                     "warehouseCode":"AVI",
-                     "poDate":"13-07-2022",
-                     "expectedArrivalDate":"13-07-2022",
-                     "otherReferences":"STCK TRS APL",
-                     "remark1":" Stock Transfer from APL 29 Jun'22\rSurat Jalan No: 9910278722, 9910278725, 9910278745, 9910278771, 9910278722",
-                     "doNo":"",
-                     "asnLine": {
-                           "inwardLineOptChar1":"0",
-                           "inwardLineOptChar2":"",
-                           "product":"151601022",
-                           "quantityOrdered":"5.000000",
-                           "uomCode":"BTL",
-                           "stockStatusCode":"NM"
-
-                    }
+                     
+                    "ownerReferences": "",
+                    "poNo": "" if record['name'] == False else record['name'],
+                    "supplierReferences": "" if record['partner_ref'] == False else record['partner_ref'],
+                    "sender": "",
+                    "documentTransCode": "" if record['x_studio_document_trans_code'] == False else record['x_studio_document_trans_code'],
+                    "ownerCode": "" if record['x_studio_owner'] == False else record['x_studio_owner'],
+                    "warehouseCode": "" if record['picking_type_id']['warehouse_id']['code'] == False else record['picking_type_id']['warehouse_id']['code'],
+                    "poDate": "" if record['date_approve'] == False else datetime.strftime(record['date_approve'], '%d/%m/%Y'),
+                    "expectedArrivalDate": "" if record['date_planned'] == False else datetime.strftime(record['date_planned'], '%d/%m/%Y'),
+                    "otherReferences": "" if record['x_studio_other_reference'] == False else record['x_studio_other_reference'],
+                    "remark1": "" if record['x_studio_remark_1'] == False else record['x_studio_remark_1'],
+                    "doNo": "" if record['x_studio_do_number'] == False else record['x_studio_do_number'],
+                    "asnLine": po_lines
                 }
             ]
         }
