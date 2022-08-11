d05d9614ef4d3220544c91964201bf1be3762cbc
yohan-kuncoro <yohan.kuncoro@bonsoftware.com>
2022-08-11 14:19:44 +0000

    rcpt

--- api_ven/controllers/controllers.py
+++ api_ven/controllers/controllers.py
@@ -237,13 +237,13 @@ class ApiVen(http.Controller):
                         line_details.append(line_detail['id'])
 
                         #Get existing receipt line data based on poNo and lineOptChar1
-#                         receipt_line = request.env['stock.move'].search(['&',('origin','=',rec['poNo']),('x_studio_opt_char_1', '=', line["inwardLineOptChar1"])])
-                        receipt_line = request.env['stock.move'].search(['&',('origin','=',rec['poNo'])])
+                        receipt_line = request.env['stock.move'].search(['&',('origin','=',rec['poNo']),('x_studio_opt_char_1', '=', line["inwardLineOptChar1"])])
+#                         receipt_line = request.env['stock.move'].search(['&',('origin','=',rec['poNo'])])
 #                 di uncommand ama fix
-                        if receipt_line['origin'] != rec['poNo']:
-                            error["Error"] = "Stock Move not found"
-                            is_error = True
-                            break
+#                         if receipt_line['origin'] != rec['poNo']:
+#                             error["Error"] = "Stock Move not found"
+#                             is_error = True
+#                             break
 
                         #Get previous receipt line detail data
                         existing_detail = []
