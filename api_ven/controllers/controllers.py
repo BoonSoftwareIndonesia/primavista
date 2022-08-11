d05c2b252e19c4f4177ee56d62e9e9038a50344e
yohan-kuncoro <yohan.kuncoro@bonsoftware.com>
2022-08-11 08:48:00 +0000

    rcpt

--- api_ven/controllers/controllers.py
+++ api_ven/controllers/controllers.py
@@ -238,7 +238,7 @@ class ApiVen(http.Controller):
 
                         #Get existing receipt line data based on poNo and lineOptChar1
                         receipt_line = request.env['stock.move'].search(['&',('origin','=',rec['poNo']),('x_studio_opt_char_1', '=', line["inwardLineOptChar1"])])
-                di uncommand ama fix
+#                 di uncommand ama fix
                         if receipt_line['origin'] != rec['poNo']:
                             error["Error"] = "Stock Move not found"
                             is_error = True
