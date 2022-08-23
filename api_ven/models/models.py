cdc0dc29eb2696ce617331f24390632ba4c9f5e2
yohan-kuncoro <yohan.kuncoro@bonsoftware.com>
2022-08-23 04:10:12 +0000

    search

--- api_ven/models/models.py
+++ api_ven/models/models.py
@@ -204,7 +204,7 @@ class ApiControllerSO(models.Model):
                 {
                     "customerPO":"",
                     "reference":"" if record['name'] == False else record['name'],
-                    "customerCode":"" if record['x_studio_customer_code'] == False else record['x_studio_customer_code'],
+                    "customerCode":"" if record['partner_shipping_id']['x_studio_customer_code'] == False else record['partner_shipping_id']['x_studio_customer_code'],
                     "soHeaderOptChar3":"",
 #                     "documentTransCode":"" if record['x_studio_document_trans_code'] == False else record['x_studio_document_trans_code'],
                     "documentTransCode":"OR", 
