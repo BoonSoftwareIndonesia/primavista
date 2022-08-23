61402d631985b30cdd3b044cce0f0c12c9a8595b
yohan-kuncoro <yohan.kuncoro@bonsoftware.com>
2022-08-23 03:30:47 +0000

    search

--- api_ven/controllers/.ipynb_checkpoints/controllers-checkpoint.py
+++ api_ven/controllers/.ipynb_checkpoints/controllers-checkpoint.py
@@ -252,8 +252,8 @@ class ApiVen(http.Controller):
 
                         #Get existing receipt line data based on poNo and lineOptChar1
 #                         receipt_line = request.env['stock.move'].search([('origin','=', rec['receiptNo'])])
-                        test = request.env['stock.move'].search([('id','=', 27)])
-                        return test['x_studio_opt_char_1']
+#                         test = request.env['stock.move'].search([('id','=', 27)])
+#                         return test['x_studio_opt_char_1']
 #                         receipt_line = request.env['stock.move'].search([('origin','=', rec['receiptNo']))
 #                         request.env['stock.move'].search([])
 
