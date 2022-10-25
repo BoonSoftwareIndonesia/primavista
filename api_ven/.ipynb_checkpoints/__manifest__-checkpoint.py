44ac8939fd0b987baa4d6f043f048c1bb8e61e1a
yohan-kuncoro <kuncoroyohan2987@gmail.com>
2022-10-25 02:10:32 +0000

    Preparation for merge to prd

--- api_ven/.ipynb_checkpoints/__manifest__-checkpoint.py
+++ api_ven/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -20,24 +20,24 @@
     'version': '0.3',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','purchase', 'sale_management', 'stock', 'crm'],
+    'depends': ['base','purchase', 'sale_management', 'stock'],
 
     # always loaded
     'data': [
         'security/ir.model.access.csv',
         'views/views.xml',
-        'views/templates.xml',
-        'views/location.xml',
+        'views/templates.xml'
+#         'views/location.xml',
     ],
     
-    'assets': {
-        'web.assets_backend': [
-            'api_ven/static/src/js/checkin_button.js',
-        ],
-        'web.assets_qweb': [
-            'api_ven/static/src/xml/checkin_button_view.xml'
-        ]
-    },
+#     'assets': {
+#         'web.assets_backend': [
+#             'api_ven/static/src/js/checkin_button.js',
+#         ],
+#         'web.assets_qweb': [
+#             'api_ven/static/src/xml/checkin_button_view.xml'
+#         ]
+#     },
     
     # only loaded in demonstration mode
     'demo': [
