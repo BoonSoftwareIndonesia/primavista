825e3599b4254efc4f4a72abafe9ddaafdc92b1c
yohan-kuncoro <yohan.kuncoro@boonsoftware.com>
2023-02-08 01:54:27 +0000

    code commented

--- api_ven/models/.ipynb_checkpoints/partner-checkpoint.py
+++ api_ven/models/.ipynb_checkpoints/partner-checkpoint.py
@@ -7,17 +7,21 @@ from odoo.exceptions import UserError
 class PartnerExt(models.Model):
     _inherit = 'res.partner'
     
-    # override fields
+    # Override fields ======================
+    # Name is required
     name = fields.Char(index=True, required=True)
+    # No duplicate x_studio_customer_id
     x_studio_customer_id = fields.Char(string='Customer ID',copy=False)
     x_studio_customer_group = fields.Char(string='Customer Group',default='IOC')
     street = fields.Char(default='NA')
     zip = fields.Char(change_default=True,default='12345')
     city = fields.Char(default='NA')
     state_id = fields.Many2one("res.country.state", string='State', ondelete='restrict', domain="[('country_id', '=?', country_id)]",required=True)
+    # Default country Indonesia
     country_id = fields.Many2one('res.country', string='Country', ondelete='restrict',default=100)
     
-    # constraints to check for duplicate x_studio_customer_id
+    
+    # Constraint to check for duplicate x_studio_customer_id ======================
     @api.constrains('x_studio_customer_id')
     def _check_x_studio_customer_id(self):
         for cust in self:
@@ -25,18 +29,23 @@ class PartnerExt(models.Model):
                 is_duplicate = False
                 is_duplicate = request.env['res.partner'].search([('id','!=',cust.id),('x_studio_customer_id', '=',cust.x_studio_customer_id)])
                 if is_duplicate:
+                    # If duplicate x_studio_customer_id exists, raise user error
                     raise UserError(('Duplicate exists: ' + cust.x_studio_customer_id))
     
-    # set copy context if duplicating existing partner
+    
+    # Set copy context if we are duplicating an existing partner ======================
     @api.returns('self', lambda value: value.id)
     def copy(self, default=None):
         if 'copy_context' not in self._context:
+            # If copy_context have not been set before
             res = super(PartnerExt, self).with_context(copy_context=True).copy(default=default)
         else:
+            # If copy_context have been set before
             res = super(PartnerExt, self).copy(default=default)
         return res
     
-    # function to set default value if null
+    
+    # Function to set default value if null ======================
     def set_default(self,partners):
         if not partners.x_studio_customer_group:
             partners.x_studio_customer_group = "IOC"
@@ -49,44 +58,58 @@ class PartnerExt(models.Model):
         if not partners.country_id:
             partners.country_id = 100
     
-    # triggers the api dw product function that sends an api log when a customer is created 
+    
+    # Triggers the api_dw_customer() function that sends an API log when creating new customer ======================
     @api.model_create_multi
     def create(self, vals_list):
         
-        # validation, internal reference and state cannot be null if we are not duplicating
+        # If we are not duplicating, x_studio_customer_id and state_id cannot be null
         if not self._context.get('copy_context'):
             if vals_list[0]['x_studio_customer_id'] is False:
                 raise UserError(('Internal reference cannot be null (partner-create)'))
                 
             if vals_list[0]['state_id'] is False:
                 raise UserError(('State cannot be null (partner-create)'))
-                
+        
+        # Call the super() method
         partners = super(PartnerExt, self).create(vals_list)
         
+        # Set default values if null
         self.set_default(partners)
         
-        # only send the data when we click import and not test
+        # Get test_import context
         test_import = self._context.get('test_import')
+        
+        # If we are not testing (test_import = False), then create customer and send API
         if not test_import:
+            # Call api_dw_customer to send API to WMS
             self.env['res.partner'].api_dw_customer(partners)
             
+            # Get the new customer's fields and values
             new_vals = {}
             for rec in partners:
                 for vals, rec in zip(vals_list, partners):
                     new_vals[rec.id] = vals
                     
-            # create activity log for partner creation
+            # Create activity log for customer creation
             self.create_activity_logs(partners, "create", new_vals = new_vals)
         return partners
     
+    
+    # Create activity log ======================
     def create_activity_logs(self, new_records, method, new_vals = {}, old_vals = {}):
+        # Assign the activity log model to a variable
         activity_log_model = self.env['api_ven.activity_log']
+        # Get the current model's name (res.partner)
         model_model = self.env[self._name]
         
+        # Get the current user's id
         curr_user_id = self._context
         curr_user_id = curr_user_id.get('uid')
+        # Get the current user's name
         curr_user_name = self.env['res.users'].browse(curr_user_id)
  
+        # Declare the values for the activity log's fields
         for rec in new_records:
             new_activity_log_vals = {
                 'method': method,
@@ -97,52 +120,66 @@ class PartnerExt(models.Model):
                 'resource_name': rec['name']
             }
 
-            # create activity log
+            # Create activity log
             new_log_id = activity_log_model.create(new_activity_log_vals)
 
-            # create the activity log line for the activity log
+            # Create the activity log line for the activity log
             if method == 'create':
+                # If we are creating a new partner, call create_log_on_create()
                 new_log_id[0].create_log_on_create(new_vals[rec['id']], self._name)
             elif method == 'write':
+                # If we are updating an existing partner, call create_log_on_write() 
                 new_log_id[0].create_log_on_write(new_vals[rec['id']], old_vals[rec['id']],self._name)
                 pass
             else:
+                # If we are deleting an existing partner, call create_log_on_unlink()
                 new_log_id[0].create_log_on_unlink(old_vals[rec['id']], self._name)
             
         return None
     
+    
+    # Triggers the api_dw_customer() function that sends an API log when updating a customer/partner ======================
     def write(self,vals):
-        # get the old vals
+        # Get the old values before update
         old_vals = {
                 rec["id"]: rec for rec in self.with_context(prefetch_fields=False).read(vals.keys())
         }
-                
+        
+        # Call the super() method
         res = super(PartnerExt, self).write(vals)
         
+        # If the write_date is not the same as create_date, that means we are doing an update
         if self.write_date != self.create_date:
-            # the result of the super() is a bool, so we need to search for the partner to pass to the api function
+            # The result of super() is a bool, so we need to search for the partner to pass to the api function
             partners = request.env['res.partner'].search([('id', '=', self.id)], limit=1)
 
+            # Get test_import context
             test_import = self._context.get('test_import')
+            
+            # Get copy_context context
             copy_context = self._context.get('copy_context')
             
-            # if we are not testing and duplicating, send api
+            # If we are not testing and duplicating, send api
             if not test_import and not copy_context:
-                # get new vals
+                # Get the new values
                 new_vals = {rec["id"]: rec for rec in self.with_context(prefetch_fields=False).read(vals.keys())}
-                # create activity log for partner update
+                
+                # Create activity log for partner update
                 self.create_activity_logs(self, "write", new_vals=new_vals, old_vals=old_vals)
-                # send api 
+                
+                # Send API to WMS 
                 self.env['res.partner'].api_dw_customer(partners)
         return res
     
+    
+    # Override customer/partner deletion to trigger the creation of an activity log when a partner is deleted ======================
     @api.ondelete(at_uninstall=False)
     def _unlink_except_user(self):
         old_vals = {}
         records = []
         
         for rec in self:
-            # get old vals
+            # Get the values of the partner that will be deleted
             old_vals[rec.id] = {
                 'x_studio_customer_id': "" if not rec.x_studio_customer_id else rec.x_studio_customer_id, 
                 'name': "" if not rec.name else rec.name, 
@@ -155,8 +192,10 @@ class PartnerExt(models.Model):
             }
             records.append({'id': rec.id, 'name': rec.name})
         
+        # Call the super() method
         super(PartnerExt, self)._unlink_except_user()
-        # create activity log for partner deletion
+        
+        # Call create_activity_logs() to trigger the creation of an activity log for partner deletion
         self.create_activity_logs(records, "unlink" , old_vals = old_vals)
         
         return None
