183724478dfc704e67aa6cfa78f973f77160f601
yohan-kuncoro <yohan.kuncoro@boonsoftware.com>
2023-03-02 02:04:37 +0000

    revert it back the generate stock compare py

--- stock_compare/wizard/.ipynb_checkpoints/generate_stock_compare-checkpoint.py
+++ stock_compare/wizard/.ipynb_checkpoints/generate_stock_compare-checkpoint.py
@@ -73,36 +73,36 @@ class CalculateOnholdWizard(models.TransientModel):
 
             stock_quants = self.env['stock.quant'].search([('product_id','=',product.id)],order="location_id desc,lot_id desc")
 
-#             for idx,stock_quant in enumerate(stock_quants):
+            for idx,stock_quant in enumerate(stock_quants):
                 
-#                 new_lot_id = stock_quant.lot_id
-#                 if new_lot_id is False:
-#                     new_lot_id = -2
+                new_lot_id = stock_quant.lot_id
+                if new_lot_id is False:
+                    new_lot_id = -2
 
-#                 # Check if stock_quant is from different lot or wh
-#                 if (new_lot_id != curr_lot) or (stock_quant.location_id != curr_loc):
-#                     # Check to create new stock line or not using prev stock quant
-#                     if (curr_lot != lot_done) and (curr_loc != loc_done):
-#                         self._create_odoo_stock_line(product, curr_wms_stock_id, stock_quants[idx-1], curr_sum)
+                # Check if stock_quant is from different lot or wh
+                if (new_lot_id != curr_lot) or (stock_quant.location_id != curr_loc):
+                    # Check to create new stock line or not using prev stock quant
+                    if (curr_lot != lot_done) and (curr_loc != loc_done):
+                        self._create_odoo_stock_line(product, curr_wms_stock_id, stock_quants[idx-1], curr_sum)
 
-#                     # Check new stock_quant exists in wms stock line or not
-#                     self._check_existing_wms_stock_line(lot_done, loc_done, product, stock_quant)
+                    # Check new stock_quant exists in wms stock line or not
+                    self._check_existing_wms_stock_line(lot_done, loc_done, product, stock_quant)
 
-#                     # Reset
-#                     curr_lot = new_lot_id
-#                     curr_loc = stock_quant.location_id
-#                     curr_sum = 0
+                    # Reset
+                    curr_lot = new_lot_id
+                    curr_loc = stock_quant.location_id
+                    curr_sum = 0
 
-#                 # If stock line already in wms stock line
-#                 if (new_lot_id == lot_done) and (stock_quant.location_id == loc_done):
-#                     continue
+                # If stock line already in wms stock line
+                if (new_lot_id == lot_done) and (stock_quant.location_id == loc_done):
+                    continue
 
-#                 #Odoo Only Stock Line
-#                 curr_sum += stock_quant.available_quantity
+                #Odoo Only Stock Line
+                curr_sum += stock_quant.available_quantity
                 
-#                 # create for last stock quant
-#                 if idx+1 == len(stock_quants):
-#                     self._create_odoo_stock_line(product, curr_wms_stock_id, stock_quants[idx], curr_sum)
+                # create for last stock quant
+                if idx+1 == len(stock_quants):
+                    self._create_odoo_stock_line(product, curr_wms_stock_id, stock_quants[idx], curr_sum)
                     
         return
 
