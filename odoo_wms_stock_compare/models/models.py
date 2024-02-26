63bf71a28eb4f5172080f670f125a12fe8ee666f
yohan-kuncoro <yohan.kuncoro@boonsoftware.com>
2024-02-26 08:38:34 +0000

    Feat: Adding lot_number and stock_status_code for return

--- odoo_wms_stock_compare/models/models.py
+++ odoo_wms_stock_compare/models/models.py
@@ -8,8 +8,8 @@ class odoo_wms_stock_compare(models.Model):
     def _compute_quantity_diff(self):
         self.odoo_wms_diff = self.odoo_quantity_on_hand - self.wms_quantity_on_hand
 
-    company_id = fields.Many2one('res.company', 'Company', required=True, index=True)
-    # company_id = fields.Many2one('res.company', 'Company', required=True, index=True, default=lambda self: self.env.company.id)
+    # company_id = fields.Many2one('res.company', 'Company', required=True, index=True)
+    company_id = fields.Many2one('res.company', 'Company', required=True, index=True, default=lambda self: self.env.company.id)
     product_id = fields.Many2one(
         'product.product', 'Product', index=True, required=True, check_company=True)
     warehouse_id = fields.Many2one('stock.warehouse', 'Warehouse', help="Technical field depicting the warehouse to consider for the route selection on the next procurement (if any).")
